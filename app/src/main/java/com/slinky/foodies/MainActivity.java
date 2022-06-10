1fa0083c4685a1f7323650bcd362af1fbd358194
francis <f.slinky07@gmail.com>
2022-06-09 20:39:53 -0400

    starting

--- app/src/main/java/com/slinky/foodies/MainActivity.java
+++ app/src/main/java/com/slinky/foodies/MainActivity.java
@@ -2,13 +2,20 @@ package com.slinky.foodies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String globalQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
